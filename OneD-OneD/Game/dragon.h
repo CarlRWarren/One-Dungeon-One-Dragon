02e107cd3975f6c68ee6d262bc646f0993069798
beastpuncher <33844960+beastpuncher@users.noreply.github.com>
2018-11-08 17:50:22 -0700

    Add files via upload

---
+++ OneD-OneD/Game/dragon.h
@@ -0,0 +1,18 @@
+#pragma once
+#pragma once
+#include "entity.h"
+#include "vector2D.h"
+#include "id.h"
+
+class Dragon : public Entity
+{
+public:
+	Dragon(Scene* scene, const ID& id) : Entity(scene, id) {}
+	~Dragon() {}
+
+	void Create(const Vector2D& position);
+	void Update();
+
+	void OnEvent(const Event & event);
+
+};
